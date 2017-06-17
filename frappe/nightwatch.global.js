e7dc1e92b8f3d62231319e29dd66a124553ab678
Faris Ansari <netchamp.faris@gmail.com>
2017-06-17 11:15:40 +0530

    console statement

--- frappe/nightwatch.global.js
+++ frappe/nightwatch.global.js
@@ -1,12 +1,14 @@
 var chromedriver = require('chromedriver');
 module.exports = {
 	before: function (done) {
+		console.log('Starting chromedriver...')
 		chromedriver.start();
 
 		done();
 	},
 
 	after: function (done) {
+		console.log('Stopping chromedriver...')
 		chromedriver.stop();
 
 		done();
